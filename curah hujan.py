e6a580b7ae2adc9a1642c4c7f0f45b5f754cd5eb
Dita Nur Rahmawati <ditanurrahmawati24@upi.edu>
2025-05-03 14:16:41 +0700

    curah hujan.py

--- curah hujan.py
+++ curah hujan.py
@@ -1,26 +1,3 @@
-#pseudocode
-print("mulai")
-print("buat list untuk menyimpan curah hujan")
-print("buat list nama bulan")
-
-print("untuk setiap bulan dalam daftar bulan:")
-print("minta input curah hujan")
-print("simpan dalam list")
-
-print("hitung total curah hujan")
-print("hitung rata-rata curah hujan")
-print("nilai tertinggi di bulan ke berapa")
-print("nilai terendah di bulan ke berapa")
-
-print("tampilkan:")
-print("total curah hujan")
-print("rata-rata curah hujan")
-print("bulan dengan hujan tertinggi")
-print("bulan dengan curah hujan terendah")
-print("selesai")
-
-
-
 from plt import image # type: ignore
 import matplotlib.pyplot as plt # type: ignore
 
